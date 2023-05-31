652eb5f534cbde306c726b320fb6469ba2e7ee14
hvkasu <kajol.sutradhar.fi>
2023-05-31 18:54:37 +0300

    Initial version of the search app

---
+++ js/script.js
@@ -0,0 +1,67 @@
+const accessKey = "YOUR_API_KEY"; // api key from unplash developer
+
+const formElement = document.querySelector("form");
+const inputElement = document.getElementById("search-input");
+const searchResults = document.querySelector(".search-results");
+const showMore = document.getElementById("show-more-button");
+
+
+let inputData = "";
+let page = 1;
+
+async function searchImages() {
+    inputData = inputElement.value;
+
+    // dynamic url from unsplash
+
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+
+    const reponse = await fetch(url);
+    const data = await reponse.json();
+
+    const results = data.results;
+
+    if (page === 1) {
+        searchResults.innerHTML = "";
+    }
+
+    // mapping
+
+    results.map((result) => {
+        // container
+        const imageWrapper = document.createElement('div');
+        imageWrapper.classList.add("search-result");
+
+        const image = document.createElement('img');
+        image.src = result.urls.small;
+        image.alt = result.alt_description;
+        const imageLink = document.createElement('a');
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+        imageLink.textContent = result.alt_description;
+
+        imageWrapper.appendChild(image);
+        imageWrapper.appendChild(imageLink);
+        searchResults.appendChild(imageWrapper);
+    });
+
+    page++;
+
+    if (page > 1) {
+        showMore.style.display = "block";
+
+    };
+
+}
+
+
+
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+    page = 1;
+    searchImages();
+});
+
+showMore.addEventListener("click", () => {
+    searchImages();
+});
